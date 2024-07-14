729833c306a552f72979ca939f983ab525639220
ujjwalX0902 <135932817+ujjwalX0902@users.noreply.github.com>
2024-07-14 15:21:49 +0530

    Add files via upload

---
+++ Lecture 4.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+	
+	int a,b,c;  //Declare the 3 variables and assign the values. 
+	a=5;b=5;c=6;
+	
+	int d=a+b-c;
+	
+	cout<<d;
+	
+	return 0;
+}
