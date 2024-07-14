9484176dc96568784d301f13f590644cbdbbec92
ujjwalX0902 <135932817+ujjwalX0902@users.noreply.github.com>
2024-07-14 15:22:58 +0530

    Add files via upload

---
+++ Lecture 5.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main(){
+//	//Declare the 2 variables and this code for the boolean statments.whether the statments is True =1; False=0;
+//	int a,b;
+//	
+//	cin>>a;
+//	cin>>b;
+//	
+//	cout<<(a>b);
+	// Next is Declare the 4 variables and this code for the and ,or operations.
+	int a,b,c,d;
+	cin>>a;
+	cin>>b;
+	cin>>c;
+	cin>>d;
+	
+	cout<<(a>b&&c>d);
+	cout<<(a>b ||c>d);
+	
+	return 0;
+}
